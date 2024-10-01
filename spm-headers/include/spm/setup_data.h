77a6efa2ab27075817dc04433483bc9f99cb4555
L <dr.trayaurus61@gmail.com>
2024-09-30 19:27:12 -0700

    npcdrv stuff

--- spm-headers/include/spm/setup_data.h
+++ spm-headers/include/spm/setup_data.h
@@ -3,6 +3,8 @@
     and handles the item-spawning side of them
 */
 
+#pragma once
+
 #include <common.h>
 #include <wii/mtx.h>
 
@@ -67,14 +69,14 @@ SIZE_ASSERT(SetupItemTemplate, 0x4)
 extern SetupItemTemplate setupItemTemplates[1]; // only contains id 0 - a coin
 
 /*
-    Takes a pointer to the item list of a setup file & its length and spawns them 
+    Takes a pointer to the item list of a setup file & its length and spawns them
 */
 void setupSpawnItems(s32 count, SetupItem * items);
 
 /*
     Takes a pointer to a setup file and returns the number of items in it, the
     version of the item data and the pointer to the array of items
-    
+
     Returns all 0s for setup files that aren't v5 or v6, or for v5 & v6 that
     don't have any item data in them
     (This reads uninitialised memory that happens to be 0 because of disc alignment)
