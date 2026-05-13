f5679fb629198b050522b7b124f412d9902929bc
Lily Lawliet <dr.trayaurus61@gmail.com>
2026-05-12 22:24:28 -0700

    remove EASTL and other changes

--- source/scripting.cpp
+++ source/scripting.cpp
@@ -197,11 +197,6 @@ s32 summonElectricity(spm::evtmgr::EvtEntry * evtEntry, bool firstRun) {
   return 2;
 }
 
-s32 activateTC(spm::evtmgr::EvtEntry * evtEntry, bool firstRun) {
-  spm::evtmgr::evtEntry(thunderCloud, 0, 0);
-  return 2;
-}
-
 s32 addCloudToList(spm::evtmgr::EvtEntry * evtEntry, bool firstRun) {
   char * tcName  = "TC";
   spm::npcdrv::NPCEntry * tc = spm::npcdrv::npcNameToPtr(tcName);
@@ -286,6 +281,18 @@ s32 npcGetPlayerVelocity(spm::evtmgr::EvtEntry * evtEntry, bool firstRun) {
   return 2;
 }
 
+  s32 evt_npc_entry_autoname(spm::evtmgr::EvtEntry * evtEntry, bool firstRun) {
+    spm::evtmgr::EvtVar *args = (spm::evtmgr::EvtVar *)evtEntry->pCurData;
+    char name[32];
+    s32 id = spm::npcdrv::npcdrv_wp->nextTemplateNpcId;
+    msl::stdio::sprintf(name,"npc_%08x", id);
+    const char * animpose = (const char*)spm::evtmgr_cmd::evtGetValue(evtEntry, args[0]);
+    spm::npcdrv::NPCEntry* entry = spm::npcdrv::npcEntry(name, animpose, args[1], 0);
+    spm::evtmgr_cmd::evtSetValue(evtEntry, args[2], (s32)entry->name);
+    return 2;
+  }
+EVT_DECLARE_USER_FUNC(evt_npc_entry_autoname, 3)
+
 spm::npcdrv::NPCTribeAnimDef animsThunderCloud[] = {
     {0, "S_1"}, // Standing (idle)
     {1, "W_1"}, // Walking
@@ -314,7 +321,6 @@ EVT_DECLARE_USER_FUNC(checkPlayerDC, 0)
 EVT_DECLARE_USER_FUNC(modWaitAnimEnd, 2)
 EVT_DECLARE_USER_FUNC(summonThunder, 0)
 EVT_DECLARE_USER_FUNC(summonElectricity, 0)
-EVT_DECLARE_USER_FUNC(activateTC, 0)
 EVT_DECLARE_USER_FUNC(addCloudToList, 0)
 EVT_DECLARE_USER_FUNC(reduce_switch_depth, 1)
 EVT_DECLARE_USER_FUNC(setScale, 1)
@@ -476,13 +482,12 @@ RETURN()
 EVT_END()
 
 EVT_BEGIN(spawnThunderCloud)
-    //USER_FUNC(spm::evt_mario::evt_mario_key_off, 0)
-    SPAWN_CHARACTER("TC", "e_kmoon", animsThunderCloud)
+    USER_FUNC(evt_npc_entry_autoname, PTR("e_kmoon"), 0, LW(14))
     USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
     ADD(LW(2), 50)
-    USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
-    USER_FUNC(spm::evt_npc::evt_npc_set_anim, PTR("TC"), 1, true)
-    USER_FUNC(activateTC)
+    USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
+    USER_FUNC(spm::evt_npc::evt_npc_set_anim, LW(14), 1, true)
+    RUN_EVT(thunderCloud)
 RETURN()
 EVT_END()
 
@@ -495,60 +500,60 @@ EVT_BEGIN(thunderCloud)
     DO(150)
       USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
       ADD(LW(2), 50)
-      USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
+      USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
       WAIT_FRM(1)
     WHILE()
-    USER_FUNC(spm::evt_npc::evt_npc_set_anim, PTR("TC"), 5, true)
-    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_BIRIBIRI1"), PTR("TC"))
+    USER_FUNC(spm::evt_npc::evt_npc_set_anim, LW(14), 5, true)
+    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_BIRIBIRI1"), LW(14))
     DO(30)
       USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
       ADD(LW(2), 50)
-      USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
+      USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
       WAIT_FRM(1)
     WHILE()
-    USER_FUNC(spm::evt_npc::evt_npc_set_anim, PTR("TC"), 1, true)
+    USER_FUNC(spm::evt_npc::evt_npc_set_anim, LW(14), 1, true)
     DO(150)
       USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
       ADD(LW(2), 50)
-      USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
+      USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
       WAIT_FRM(1)
     WHILE()
-    USER_FUNC(spm::evt_npc::evt_npc_set_anim, PTR("TC"), 5, true)
-    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_BIRIBIRI1"), PTR("TC"))
+    USER_FUNC(spm::evt_npc::evt_npc_set_anim, LW(14), 5, true)
+    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_BIRIBIRI1"), LW(14))
     DO(30)
       USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
       ADD(LW(2), 50)
-      USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
+      USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
       WAIT_FRM(1)
     WHILE()
-    USER_FUNC(spm::evt_npc::evt_npc_set_anim, PTR("TC"), 2, true)
+    USER_FUNC(spm::evt_npc::evt_npc_set_anim, LW(14), 2, true)
     DO(180)
       USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
       ADD(LW(2), 50)
-      USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
+      USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
       WAIT_FRM(1)
     WHILE()
-    USER_FUNC(spm::evt_npc::evt_npc_set_anim, PTR("TC"), 5, true)
-    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_BIRIBIRI1"), PTR("TC"))
+    USER_FUNC(spm::evt_npc::evt_npc_set_anim, LW(14), 5, true)
+    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_BIRIBIRI1"), LW(14))
     DO(30)
       USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
       ADD(LW(2), 50)
-      USER_FUNC(spm::evt_npc::evt_npc_set_position, PTR("TC"), LW(1), LW(2), LW(3))
+      USER_FUNC(spm::evt_npc::evt_npc_set_position, LW(14), LW(1), LW(2), LW(3))
       WAIT_FRM(1)
     WHILE()
     USER_FUNC(spm::evt_mario::evt_mario_key_off, 0)
     USER_FUNC(spm::evt_mario::evt_mario_get_pos, LW(1), LW(2), LW(3))
-    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_THUNDER1"), PTR("TC"))
+    USER_FUNC(spm::evt_snd::evt_snd_sfxon_npc, PTR("SFX_I_THUNDER1"), LW(14))
     USER_FUNC(summonThunder)
     USER_FUNC(summonElectricity)
     //USER_FUNC(spm::evt_eff::evt_eff, 0, PTR("jigen_paralysis"), 0, LW(1), LW(2), LW(3), 0, 0, 0, 0, 0, 0, 0, 0)
     USER_FUNC(setScale, FLOAT(0.5))
     WAIT_MSEC(1000)
     USER_FUNC(spm::evt_mario::evt_mario_key_on)
-    USER_FUNC(spm::evt_npc::evt_npc_get_position, PTR("TC"), LW(0), LW(1), LW(2))
+    USER_FUNC(spm::evt_npc::evt_npc_get_position, LW(14), LW(0), LW(1), LW(2))
     USER_FUNC(spm::evt_eff::evt_eff, 0, PTR("dmen_warp"), 0, LW(0), LW(1), LW(2), 0, 0, 0, 0, 0, 0, 0, 0)
     USER_FUNC(spm::evt_snd::evt_snd_sfxon_3d, PTR("SFX_BS_DMN_GOOUT1"), LW(0), LW(1), LW(2))
-    USER_FUNC(spm::evt_npc::evt_npc_delete, PTR("TC"))
+    USER_FUNC(spm::evt_npc::evt_npc_delete, LW(14))
     WAIT_MSEC(15000)
     USER_FUNC(setScale, FLOAT(1.0))
 RETURN()
